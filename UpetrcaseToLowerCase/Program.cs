c4796e42332cbc969a78b9e981d7f53232c037a0
abhay <abhay@DESKTOP-2G49AHH>
2022-05-03 01:46:47 +0530

    Add project files.

---
+++ UpetrcaseToLowerCase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpetrcaseToLowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a upper case Character");
+
+            Char value = Convert.ToChar(Console.ReadLine());
+
+         //   value = Convert.ToChar(index_number);
+
+            Console.WriteLine("The input value is  {0} ", value);
+
+            int unicode = value + 32;
+
+            Char myChar = (Char)unicode;
+
+            Console.WriteLine("The required Output is {0}",myChar);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
